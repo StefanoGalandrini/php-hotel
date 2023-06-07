34819d0ea5a3ac1a1998a03a60578717164d1930
StefanoGalandrini <stefe68@gmail.com>
2023-06-07 17:09:08 +0200

    Add filter logic for both parking and rating

--- index.php
+++ index.php
@@ -1,42 +1,65 @@
 <?php
 
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
+$hotels = [
+		[
+				'name' => 'Hotel Belvedere',
+				'description' => 'Hotel Belvedere Descrizione',
+				'parking' => true,
+				'vote' => 4,
+				'distance_to_center' => 10.4
+		],
+		[
+				'name' => 'Hotel Futuro',
+				'description' => 'Hotel Futuro Descrizione',
+				'parking' => true,
+				'vote' => 2,
+				'distance_to_center' => 2
+		],
+		[
+				'name' => 'Hotel Rivamare',
+				'description' => 'Hotel Rivamare Descrizione',
+				'parking' => false,
+				'vote' => 1,
+				'distance_to_center' => 1
+		],
+		[
+				'name' => 'Hotel Bellavista',
+				'description' => 'Hotel Bellavista Descrizione',
+				'parking' => false,
+				'vote' => 5,
+				'distance_to_center' => 5.5
+		],
+		[
+				'name' => 'Hotel Milano',
+				'description' => 'Hotel Milano Descrizione',
+				'parking' => true,
+				'vote' => 2,
+				'distance_to_center' => 50
+		],
+];
+
+// GET values and returns filtered array
+$park = isset($_GET['park']) && $_GET['park'] != "" ? $_GET['park'] : null;
+$vote = isset($_GET['vote']) && $_GET['vote'] != "" ? $_GET['vote'] : null;
+
+$filtered_hotels = array_filter($hotels, function($hotel) use ($park, $vote) {
+	$hotel["parking"] ? $parking = "Yes" : $parking = "No";  // Convert hotel parking boolean to Yes/No string
+
+	// If park is set, check if it matches the hotel's parking availability
+	if ($park !== null && $parking != $park) {
+		return false;
+	}
+	// If vote is set, check if it matches the hotel's rating
+	if ($vote !== null && $hotel["vote"] != $vote) {
+		return false;
+	}
+	// If none of the above conditions are met, include the hotel in the filtered array
+	return true;
+});
+
+// reset the array keys
+$filtered_hotels = array_values($filtered_hotels); 
+
 
 ?>
 
@@ -60,34 +83,70 @@
 </head>
 
 <body>
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th scope="col">#</th>
-				<th scope="col">Name</th>
-				<th scope="col">Parking</th>
-				<th scope="col">Rating</th>
-				<th scope="col">Distance to Center</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-		foreach ($hotels as $key => $hotel){ 
-		$hotel["parking"] ? $parking = "Yes" : $parking = "No";
-		?>
+	<div class="container" style="width: 70vw">
+		<h1 class="mb-5" style="text-align: center">HOTELS</h1>
 
-			<tr>
-				<th scope="row"><?= $key + 1 ?></th>
-				<td><?=$hotel["name"]?></td>
-				<td><?=$parking?></td>
-				<td><?=$hotel["vote"]?></td>
-				<td><?=$hotel["distance_to_center"]?></td>
-			</tr>
-			<?php
+		<!-- Form with input fields to filter Hotels -->
+		<form action="" method="get" class="mb-5 mx-auto" style="width: 30%">
+			<label for="park" class="form-label">Parcheggio disponibile</label>
+			<select id="park" name="park" class="form-select form-select-sm mb-3"
+				aria-label=".form-select-sm example">
+				<option selected value="">Seleziona un'opzione</option>
+				<option value="Yes">Sì</option>
+				<option value="No">No</option>
+			</select>
+			<label for="vote" class="form-label">Punteggio</label>
+			<select id="vote" name="vote" class="form-select form-select-sm mb-3"
+				aria-label=".form-select-sm example">
+				<option selected value="">Seleziona un'opzione</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+			</select>
+			<div class="mx-auto" style="width: 100%; text-align: center">
+				<button class="btn btn-success me-5" type="submit">Filtra</button>
+				<button class="btn btn-warning" type="reset">Cancella</button>
+			</div>
+		</form>
+
+
+
+		<!-- Table to display hotels information -->
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Name</th>
+					<th scope="col">Parking</th>
+					<th scope="col">Rating</th>
+					<th scope="col">Distance to Center</th>
+				</tr>
+			</thead>
+			<tbody>
+
+				<!-- Loop in filtered array -->
+				<?php
+				foreach ($filtered_hotels as $key => $hotel){ 
+
+				// Variable to convert boolean to text
+				$hotel["parking"] ? $parking = "Yes" : $parking = "No";
+				?>
+
+				<tr>
+					<th scope="row"><?= $key + 1 ?></th>
+					<td><?=$hotel["name"]?></td>
+					<td><?=$parking?></td>
+					<td><?=$hotel["vote"]?></td>
+					<td><?=$hotel["distance_to_center"]?></td>
+				</tr>
+				<?php
 		}
 		?>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	</div>
 </body>
 
 </html>
